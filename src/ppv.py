5b57169824892f5c790835ba158a0456b22fcd50
Ben Crestel <ben.crestel@zoho.com>
2020-10-09 15:37:05 -0400

    typing + format

--- src/ppv.py
+++ src/ppv.py
@@ -1,10 +1,12 @@
 # ppv.py
 # Benjamin Crestel, 2020-10-09
 
-def calculate_ppv(percentage_experiments_H0true: float,
-        alpha_level: float,
-        power: float
-        ):
+from typing import Dict
+
+
+def calculate_ppv(
+    percentage_experiments_H0true: float, alpha_level: float, power: float
+) -> Dict[str, float]:
     """
     Calculate the Positive Predictive Value (PPV) of a p-value
 
@@ -13,11 +15,10 @@ def calculate_ppv(percentage_experiments_H0true: float,
     :param power: power of the study
     :return:
     """
-    TP = (1.0-percentage_experiments_H0true) * power
-    FN = (1.0-percentage_experiments_H0true) * (1.0 - power)
+    TP = (1.0 - percentage_experiments_H0true) * power
+    FN = (1.0 - percentage_experiments_H0true) * (1.0 - power)
     FP = percentage_experiments_H0true * alpha_level
     TN = percentage_experiments_H0true * (1.0 - alpha_level)
     PPV = TP / (TP + FP)
     FPRP = FP / (TP + FP)
-    return {'TP':TP, 'FN':FN, 'FP':FP, 'TN':TN, 'PPV':PPV, 'FPRP':FPRP}
-
+    return {"TP": TP, "FN": FN, "FP": FP, "TN": TN, "PPV": PPV, "FPRP": FPRP}
