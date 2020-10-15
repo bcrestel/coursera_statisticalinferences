d82856a59cf48dc91e8978ccadaad1488ccf8e65
Ben Crestel <ben.crestel@zoho.com>
2020-10-14 23:23:54 -0400

    add p_hacking and swith to prop H1 true

--- src/ppv.py
+++ src/ppv.py
@@ -5,20 +5,27 @@ from typing import Dict
 
 
 def calculate_ppv(
-    percentage_experiments_H0true: float, alpha_level: float, power: float
+    percentage_experiments_H1true: float, alpha_level: float, power: float, p_hacking_ratio: float = 0.0
 ) -> Dict[str, float]:
     """
     Calculate the Positive Predictive Value (PPV) of a p-value
 
-    :param percentage_experiments_H0true: percentage of experiments for which the null is true
+    :param percentage_experiments_H1true: percentage of experiments for which the alternative hypothesis is true
     :param alpha_level: significance level
     :param power: power of the study
+    :param p_hacking_ratio: percentage of negative results presented as positive results
     :return:
     """
-    TP = (1.0 - percentage_experiments_H0true) * power
-    FN = (1.0 - percentage_experiments_H0true) * (1.0 - power)
-    FP = percentage_experiments_H0true * alpha_level
-    TN = percentage_experiments_H0true * (1.0 - alpha_level)
+    TP = percentage_experiments_H1true * power
+    FN = percentage_experiments_H1true * (1.0 - power)
+    TP += p_hacking_ratio * FN
+    FN = (1.0 - p_hacking_ratio) * FN
+
+    FP = (1.0 - percentage_experiments_H1true) * alpha_level
+    TN = (1.0 - percentage_experiments_H1true) * (1.0 - alpha_level)
+    FP += p_hacking_ratio * TN
+    TN = (1.0 - p_hacking_ratio) * TN
+
     PPV = TP / (TP + FP)
     FPRP = FP / (TP + FP)
     return {"TP": TP, "FN": FN, "FP": FP, "TN": TN, "PPV": PPV, "FPRP": FPRP}
